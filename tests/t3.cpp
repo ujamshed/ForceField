a04ac24508ea9ca16552e5ec7a884bdc7d2b7841
Usman Jamshed <usman.jamshedk@gmail.com>
2022-12-18 18:52:11 -0500

    Done MVP can work on improvements now such as calculating angles and diehdrals on the fly instead of passing them in

---
+++ tests/t3.cpp
@@ -0,0 +1,26 @@
+#include "../include/input.h"
+#include "../include/utility.h"
+#include "../include/energy.h"
+
+int main()
+{
+    std::cout << "First Molecule: " << std::endl;
+    
+    Molecule methane("../data/methane3.txt");
+    
+    // Get input methane coordinates
+    arma::mat Initial_Methane_Coordinates = methane.getMoleculeCoordinates();
+
+    // Calculate initial methane energy
+    calcEnergy Methane_energy(&methane);
+    std::cout << "Initial Methane Energy: " << std::endl;
+    std::cout << Methane_energy.total(Initial_Methane_Coordinates, false) << std::endl;
+
+    // Export initial methane coordinates (to compare with output)
+    Methane_energy.export_sdf(Initial_Methane_Coordinates, "methane3_input");
+
+    // Optimize structure and output final energy and export the final output
+    arma::mat output = Methane_energy.BFGS(0.0001);
+    Methane_energy.total(output);
+    Methane_energy.export_sdf(output, "Methane3_out");
+}
